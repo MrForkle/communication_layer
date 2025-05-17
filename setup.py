b468976ee161a4838c3156686d22a9ed3a82310a
MrForkle <Aaron_Makes_Games@proton.me>
2025-05-17 19:51:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='adrts_comm_layer',
       description='the communication layer for attack defend rts, includes the postgres interface as well as other server related functions',
       author='MrForkle',
       url='https://github.com/MrForkle/Attack_Defend_RTS_Communication_Layer',
-      packages=['psycopg2',],
+      install_requires=['psycopg2',],
      )
