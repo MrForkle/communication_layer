c8f7665a01812f3b304287d7b818d30d9ff61f0c
MrForkle <Aaron_Makes_Games@proton.me>
2025-05-17 19:51:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from distutils.core import setup
 
 setup(name='adrts_comm_layer',
-      version='1.0',
       description='the communication layer for attack defend rts, includes the postgres interface as well as other server related functions',
       author='MrForkle',
       url='https://github.com/MrForkle/Attack_Defend_RTS_Communication_Layer',
