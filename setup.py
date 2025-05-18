78358c3f464cdba66826b2183976f539b69b6b9d
MrForkle <Aaron_Makes_Games@proton.me>
2025-05-18 19:17:48 -0400

    now adds directory to sys when installed

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='adrts_comm_layer',
       description='the communication layer for attack defend rts, includes the postgres interface as well as other server related functions',
       author='MrForkle',
       url='https://github.com/MrForkle/Attack_Defend_RTS_Communication_Layer',
       install_requires=['psycopg2-binary',],
-      packages=find_packages(),
+      packages=['communication_layer',],
      )
