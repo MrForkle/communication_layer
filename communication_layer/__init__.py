4bbe5f9b7d5a80619347b1173946c66e035c01a3
MrForkle <Aaron_Makes_Games@proton.me>
2025-05-17 20:07:12 -0400

    fixed __init_

--- communication_layer/__init__.py
+++ communication_layer/__init__.py
@@ -1,1 +1,6 @@
-import communication_layer
+# Define the __all__ variable
+__all__ = ["communication_layer", "jwt_tokens"]
+
+# Import the submodules
+from . import communication_layer
+from . import jwt_tokens
