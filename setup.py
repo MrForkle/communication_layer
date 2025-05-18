ac93b5ceb1221122398cd116b552dd202aea7f93
MrForkle <Aaron_Makes_Games@proton.me>
2025-05-18 19:14:41 -0400

    used find packages in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(name='adrts_comm_layer',
       description='the communication layer for attack defend rts, includes the postgres interface as well as other server related functions',
       author='MrForkle',
       url='https://github.com/MrForkle/Attack_Defend_RTS_Communication_Layer',
       install_requires=['psycopg2-binary',],
-      packages=['communication_layer',],
+      packages=find_packages(),
      )
