576e01aa598f651f5d0e1cef6393e0b802746e9d
MrForkle <Aaron_Makes_Games@proton.me>
2025-05-20 18:52:27 -0400

    removed package parts

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='adrts_comm_layer',
-      description='the communication layer for attack defend rts, includes the postgres interface as well as other server related functions',
-      author='MrForkle',
-      url='https://github.com/MrForkle/Attack_Defend_RTS_Communication_Layer',
-      install_requires=['psycopg2-binary',],
-      packages=['communication_layer',],
-     )
